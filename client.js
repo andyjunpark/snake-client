a8f0db36283f6a0e0fbbe800c972f25232d29b42
Andy Junhyuk Park <pos03142ap@gmail.com>
2021-12-01 23:53:34 +0000

    added move up by hardcoding

--- client.js
+++ client.js
@@ -11,6 +11,7 @@ const connect = function () {
   conn.setEncoding("utf8");
   conn.on("connect", () => {
     conn.write("Name: AJP");    // code that does something when the connection is first established
+    // conn.write("Move: up");
   });
   
   return conn;
