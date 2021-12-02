577f0fe95df86e9c785a41a14be6b31c1b2af1bf
Andy Junhyuk Park <pos03142ap@gmail.com>
2021-12-02 00:26:57 +0000

    implemented movements to write on server

--- input.js
+++ input.js
@@ -1,4 +1,17 @@
-const setupInput = function () {
+// Stores the active TCP connection object.
+let connection;
+
+// const setupInput = function () {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//   stdin.on("data", handleUserInput);
+//   return stdin;
+// };
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,7 +23,15 @@ const setupInput = function () {
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
-  }// your code here
+  } else if (key === 'w') {
+    connection.write("Move: up");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  } else if (key === 's') {
+    connection.write("Move: down");
+  } else if (key === 'd') {
+    connection.write("Move: right");
+  }
 };
 
 setupInput();
